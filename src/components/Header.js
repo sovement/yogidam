0c3c248bde27d5e6287dd6c8ebe75a9c8a5fac20
csechrome <csechrome@gmail.com>
2022-10-09 17:19:59 +0900

    Fix: Side Menu 쏠림 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,39 +16,37 @@ const Header = () => {
                 <img src="./images/ic_menu.svg" />
             </label>
             <div id="sideMenu">
-                <ul>
-                    <Link to="/mypage" style={{ textDecoration: 'none', color: 'black' }}>
-                        <div class="tabProfile">
-                            {/* <div width={80} height={80}> */}
-                            <img className="profilePicture" src="./images/app_symbol.svg" />
-                            로그인하세요
-                            {/* </div> */}
-                        </div>
-                    </Link>
+                <Link to="/mypage" style={{ textDecoration: 'none', color: 'black' }}>
+                    <div class="tabProfile">
+                        {/* <div width={80} height={80}> */}
+                        <img className="profilePicture" src="./images/app_symbol.svg" />
+                        로그인하세요
+                        {/* </div> */}
+                    </div>
+                </Link>
 
-                    <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
-                        <div class="tab">
-                            <img className="tabIcon" src="./images/ic_map.svg" />
-                            지도
-                        </div>
-                    </Link>
+                <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
                     <div class="tab">
-                        <img className="tabIcon" src="./images/ic_reward.svg" />
-                        리워드 프로모션
+                        <img className="tabIcon" src="./images/ic_map.svg" />
+                        지도
                     </div>
-                    <div class="tab">
-                        <img className="tabIcon" src="./images/ic_chat.svg" />
-                        민원 신청
-                    </div>
-                    <div class="tab">
-                        <img className="tabIcon" width={24} height={24} src="./images/app_symbol.svg" />
-                        About
-                        <img width={79} height={11} src="./images/app_logo_sovement.svg" />
-                    </div>
-                    <div>
-                        
-                    </div>
-                </ul>
+                </Link>
+                <div class="tab">
+                    <img className="tabIcon" src="./images/ic_reward.svg" />
+                    리워드 프로모션
+                </div>
+                <div class="tab">
+                    <img className="tabIcon" src="./images/ic_chat.svg" />
+                    민원 신청
+                </div>
+                <div class="tab">
+                    <img className="tabIcon" src="./images/app_symbol.svg" />
+                    About&nbsp;
+                    <img width={80} height={12} src="./images/app_logo_sovement.svg" />
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
     );
