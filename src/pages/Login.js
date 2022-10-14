0c045eab515f034968712bf3b50a24270680d6f6
2021147562 <kim2ju@yonsei.ac.kr>
2022-10-15 07:12:47 +0900

    민원

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
         const body = {
             grant_type: "authorization_code",
             client_id: process.env.REACT_APP_KAKAO_REST_API_KEY,
-            redirect_uri: "http://yogidam.com/login",
+            redirect_uri: "https://yogidam.com/login",
             code: authorizeCodeFromKakao
         }
 
@@ -106,7 +106,7 @@ const Login = () => {
     }
 
     const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
-    const REDIRECT_URI = 'http://yogidam.com/login';
+    const REDIRECT_URI = 'https://yogidam.com/login';
     const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 
