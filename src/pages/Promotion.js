27f9eb255505721122f8631db7564e62cd8666ee
csechrome <csechrome@gmail.com>
2022-10-26 20:17:59 +0900

    Fix: 반응형 디자인 추가

--- src/pages/Promotion.js
+++ src/pages/Promotion.js
@@ -5,14 +5,20 @@ const Promotion = () => {
     return (
         <>
             <Header />
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <img className='promotionImg' src='./images/image_promotion_1.png' />
-                <img className='promotionImg' src='./images/image_promotion_2.png' />
-                <img className='promotionImg' src='./images/image_promotion_3.png' />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'center', backgroundColor: '#000000' }}>
+                    <img className='promotionImg' src='./images/image_promotion_1.png' />
+                </div>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'center', backgroundColor: '#f1f1f1' }}>
+                    <img className='promotionImg' src='./images/image_promotion_2.png' />
+                </div>
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'center', backgroundColor: '#ffffff' }}>
+                    <img className='promotionImg' src='./images/image_promotion_3.png' />
+                </div>
 
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'center' }}>
                     <img style={{ marginBottom: '52px' }} className='promotionNotice' src='./images/image_promotion_4.png' />
-                    <img className='btnKakaoChannel' src='./images/ic_kakaochannel.png' />
+                    <div className='btnKakaoChannel' />
                 </div>
             </div>
         </>
