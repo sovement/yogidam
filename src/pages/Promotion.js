6d80c5bddcf9d29872b5c870ba33c19e1cf96501
csechrome <csechrome@gmail.com>
2022-10-26 19:49:00 +0900

    Feat: 프로모션 페이지 오픈

--- src/pages/Promotion.js
+++ src/pages/Promotion.js
@@ -5,12 +5,15 @@ const Promotion = () => {
     return (
         <>
             <Header />
-            <div style={{ display: 'flex', flexDirection: 'column'}}>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <img className='promotionImg' src='./images/image_promotion_1.png' />
                 <img className='promotionImg' src='./images/image_promotion_2.png' />
                 <img className='promotionImg' src='./images/image_promotion_3.png' />
-                <img className='promotionNotice' src='./images/image_promotion_4.png' />
-                <img className='btnKakaoChannel' src='./images/ic_kakaochannel.png' />
+
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <img style={{ marginBottom: '52px' }} className='promotionNotice' src='./images/image_promotion_4.png' />
+                    <img className='btnKakaoChannel' src='./images/ic_kakaochannel.png' />
+                </div>
             </div>
         </>
     );
